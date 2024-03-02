d5015f96df9d79946dd85dc37903124fbc83d82f
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2024-03-02 14:35:55 -0500

    implemented authentication

---
+++ frontend/arthas-ai/app/login/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import page from "../page";
+import { loginSchema } from "../utils/forms/zodTypes";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { createClient } from "../utils/supabase/client";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
+import { navigate } from "./actions";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    values: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const supabase = createClient();
+
+  const onSubmit = async (data: z.infer<typeof loginSchema>) => {
+    console.log(data);
+    await supabase.auth
+      .signInWithPassword({
+        email: data.email,
+        password: data.password,
+      })
+      .then((response) => {
+        if (response.error) {
+          console.log(response.error);
+        } else {
+          navigate();
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Login</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
+
+export default Login;
