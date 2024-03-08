96ddf752aea492dfbf5d6eeed59849f68d1ce13d
Valentina Rojas <valentinaxrojas@gmail.com>
2024-03-08 16:06:22 -0500

    Added skeleton code for team page

---
+++ frontend/arthas-ai/app/teampage/page.tsx
@@ -0,0 +1,13 @@
+// Resources.tsx
+import React from "react";
+import Link from "next/link";
+
+const Teampage = () => {
+	return (
+		<div className="">
+			<h1>This is the Team Page!</h1>
+		</div>
+	);
+};
+
+export default Teampage;
