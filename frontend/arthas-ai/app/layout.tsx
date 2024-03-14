87bbebbf6a46fa7525dc3482578fc4aef6227e3f
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2024-03-13 21:56:21 -0400

    created auth provider for authentication

--- frontend/arthas-ai/app/layout.tsx
+++ frontend/arthas-ai/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "./utils/providers/reactQueryProvider";
+import AuthProvider from "./utils/providers/authProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <AuthProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </AuthProvider>
       </body>
     </html>
   );
