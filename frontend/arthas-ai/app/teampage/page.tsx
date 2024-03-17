e81ac52bf2c42e237fcbb96df97b149f48e54788
Valentina Rojas <valentinaxrojas@gmail.com>
2024-03-17 00:13:03 -0400

    Created Team Card Component

--- frontend/arthas-ai/app/teampage/page.tsx
+++ frontend/arthas-ai/app/teampage/page.tsx
@@ -1,13 +1,226 @@
 // Resources.tsx
 import React from "react";
 import Link from "next/link";
+import Teamcard from "../components/Teamcard";
+import Head from 'next/head'
 
-const Teampage = () => {
+import { 
+	Card, 
+	CardContent, 
+	CardDescription, 
+	CardFooter, 
+	CardHeader, 
+	CardTitle 
+  } from '@/components/ui/card'
+
+{/* 2 Members */}
+const teamLeads = [
+	{
+		id: "1",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "2",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	}
+]
+
+{/* 10 Members */}
+const frontend = [
+	{
+		id: "3",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "4",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "5",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "6",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "7",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "8",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "9",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "10",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "11",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "12",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	}
+]
+
+{/* 5 Members */}
+const backend = [
+	{
+		id: "13",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "14",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "15",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "16",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	},
+	{
+		id: "17",
+		image: "https://i.pinimg.com/originals/89/79/90/897990126794575981766f3c71324152.jpg",
+		name: "CHUU",
+		role: "Frontend Developer",
+		quote: "I worked on...",
+		github: "http://github.com/gabsebas",
+		linkedin: "http://linkedin."
+	}
+]
+
+  const TeamPage = () => {
 	return (
-		<div className="">
-			<h1>This is the Team Page!</h1>
-		</div>
+	  <>
+	  	<Head>
+		  <title>Meet the Arthas AI Team!</title>
+		</Head>
+		<header className="bg-gray-100 p-6">NavBar (GIAN PLEASE HELP)</header>
+		<main>
+			{/* Banner */}
+			<div className="p-10 w-1440 h-302.093 bg-blue-100">
+				<h1 className="text-center font-medium p-4 text-4xl">Meet The Arthas AI Team!</h1>
+				<p className="p-4 text-center text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			{/* Team Leads */}
+			<div className="flex flex-col items-center justify-center p-8">
+				<h1 className="text-center text-3xl font-medium">Team Leads</h1>
+				{/* Team Leads Grid */}
+				<div className="grid grid-cols-2 gap-12 max-w-6xl pt-10">
+					{teamLeads.map((lead) => (
+              			<Teamcard key={lead.id} {...lead} />
+            		))}
+				</div>
+			</div>
+			{/* Frontend Team */}
+			<div className="flex flex-col h-full items-center justify-center p-8">
+				<h1 className="text-center text-3xl font-medium">Frontend Team</h1>
+				{/* Frontend Team Grid */}
+				<div className="grid grid-cols-5 grid-rows-2 gap-12 max-w-6xl pt-10">
+					{frontend.map((frontend) => (
+              			<Teamcard key={frontend.id} {...frontend} />
+            		))}
+				</div>
+			</div>
+			{/* Backend Team */}
+			<div className="p-10">
+				<h1 className="text-center text-3xl font-medium">Backend Team</h1>
+				{/* Backend Team Grid */}
+				
+			</div>
+		</main>
+	  </>
 	);
-};
-
-export default Teampage;
+  };
+  
+  export default TeamPage;
+  
