aa1c1dec2a2cb8f4849df8989f30ae53e2de6f1c
gabsebas <smolgaby27@gmail.com>
2024-03-17 20:57:08 -0400

    added faq hyperlink that scrolls to section

--- frontend/arthas-ai/app/about/page.tsx
+++ frontend/arthas-ai/app/about/page.tsx
@@ -19,7 +19,7 @@ const About = () => {
 
       <main>
         {/* Banner */}
-        <div className="p-10 w-1440px h-291px bg-pink-100">
+        <div className="p-10 w-1440px h-291px bg-custom-gradient">
           <h1 className="text-center font-medium text-3xl pb-4">
             {" "}
             About Arthas AI
@@ -33,13 +33,51 @@ const About = () => {
 
         <div className="flex w-full min-h-screen">
           {/* SideBar */}
-          <div className="w-1/3 h-screen p-4 bg-gray-100">
+          <div className="w-1/5 p-4 bg-white border-r-2">
             <h2 className="text-center font-medium text-2xl"> Overview </h2>
+              <Link href="#faq" className="text-center">FAQ</Link>
           </div>
 
           {/* Main Content */}
-          <div className="w-2/3 p-4 bg-blue-100">
+          <div className="w-4/5 p-4 bg-white">
+            <h2 id="mission" className="text-left font-medium text-2xl pb-4 px-4">
+              {" "}
+              Mission{" "}
+            </h2>
+            <p className="text-left pb-4 px-4">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+
             <h2 className="text-left font-medium text-2xl pb-4 px-4">
+              {" "}
+              Features{" "}
+            </h2>
+            <p className="text-left pb-4 px-4">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+
+            <h2 className="text-left font-medium text-2xl pb-4 px-4">
+              {" "}
+              Audience{" "}
+            </h2>
+            <p className="text-left pb-4 px-4">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+
+            {/* Extra Sections to test sidebar works*/}
+            <h2 id="mission" className="text-left font-medium text-2xl pb-4 px-4">
               {" "}
               Mission{" "}
             </h2>
@@ -76,13 +114,19 @@ const About = () => {
             </p>
 
             {/* FAQ Section */}
-            <h2 className="text-left font-medium text-2xl px-4"> FAQ </h2>
-            <Accordion className= "pb-4 px-4" type="single" collapsible>
-              <AccordionItem value="item-1">
+            <h2 id = "faq" className="text-left font-medium text-2xl px-4 pb-4" > FAQ </h2>
+            <Accordion className= "pb-4 px-4 " type="single" collapsible>
+              <AccordionItem className = "border-box border-gray-200 rounded-lg shadow-lg mb-2 pb-2 px-4" value="item-1">
                 <AccordionTrigger>Is it accessible?</AccordionTrigger>
                 <AccordionContent>
                   Yes. It adheres to the WAI-ARIA design pattern.
                 </AccordionContent>
+              </AccordionItem>
+              <AccordionItem className = "border-box rounded-2xl shadow-lg p-4 px-4" value="item-2"> 
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                  </AccordionContent>
               </AccordionItem>
             </Accordion>
           </div>
