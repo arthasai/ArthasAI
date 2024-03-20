f9f4531f2973f1cf4c36c915bd7812d20c22c1a9
richardbrrito <richardbrito116@gmail.com>
2024-03-20 12:37:32 -0400

    TextArea resize
    Changed css to put back textArea vertical resize

--- frontend/arthas-ai/app/components/chatInterface.tsx
+++ frontend/arthas-ai/app/components/chatInterface.tsx
@@ -106,7 +106,7 @@ const ChatInterface = () => {
       </div>
       <div className="flex justify-between items-center pb-1 pl-1 pt-[0.1rem]">
         <textarea /* input for user to type message */
-          className="flex h-10 w-full resize-none rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+          className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
           // basic border, removes resize from textarea ( can be added again if wanted, just remove resize-none)
           placeholder="Type your message here"
           value={inputText}
