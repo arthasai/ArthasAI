b632a16522e97d2295b62703ae394965e4f0a23d
David Moreno <davidmoreno1314@gmail.com>
2024-03-21 23:10:53 -0400

    "Replace `<Navbar></Navbar>` with `<Navbar/>` in layout.tsx"

--- frontend/arthas-ai/app/layout.tsx
+++ frontend/arthas-ai/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
+        <Navbar/>
         <AuthProvider>
           <ReactQueryProvider>{children}</ReactQueryProvider>
         </AuthProvider>
