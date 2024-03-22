e51cf45aff1eeb2f66e259b44f9b38cf29cdc730
David Moreno <davidmoreno1314@gmail.com>
2024-03-21 23:08:25 -0400

    navBar in layout

--- frontend/arthas-ai/app/components/Navbar.tsx
+++ frontend/arthas-ai/app/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                     <div className="flex items-center justify-between h-16">
                         <div className="flex items-center">
                             <div className="flex-shrink-0">
-                                <a href="/" className="text-white">
+                                <a href="/login" className="text-white">
                                     <BoxIcon></BoxIcon> 
                                     {/**LOGO GOES HERE */}
                                 </a>
