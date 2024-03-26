5b2f21d246a115726f1f62d527c146832532d751
Valentina Rojas <valentinaxrojas@gmail.com>
2024-03-25 21:46:19 -0400

    Trying to fix quotation eror

--- frontend/arthas-ai/app/about/page.tsx
+++ frontend/arthas-ai/app/about/page.tsx
@@ -20,9 +20,11 @@ const About = () => {
         {/* Banner */}
         <div className="p-10 w-1440px h-291px bg-custom-gradient">
           <h1 className="text-center font-medium text-3xl pb-4">
+            {" "}
             About Arthas AI
           </h1>
           <p className="text-center font-sm pb-4">
+            {" "}
             Curious about something? Use the Arthas AI search engine to find research papers based on your curiousity.
           </p>
         </div>
@@ -44,18 +46,22 @@ const About = () => {
           {/* Main Content */}
           <div className="bg-white p-6 w-full md:w-4/5 ml-6 mt-2 mr-6">
             <h2 id="section-1" className="text-left font-medium text-2xl pb-4">
+              {" "}
               What is Arthas AI?
             </h2>
             <p className="text-left pb-4">
+              {" "}
               Arthas AI is an AI search engine platform created from scratch focused on efficiently searching for AI research papers. 
               It uses an AI first approach towards building a search engine that can more intelligently find, organize, 
               and deliver the information that piques our curiosity.
             </p>
 
             <h2 id = "section-2" className="text-left font-medium text-2xl pb-4">
+              {" "}
               Our Mission
             </h2>
             <p className="text-left pb-4">
+              {" "}
               Since the 1990s, search engines have revolutionized access to information. 
               Yet, three decades on, their fundamental mechanics have scarcely evolved. 
               With Arthas AI, we're pioneering a search engine that transcends
@@ -65,9 +71,11 @@ const About = () => {
             </p>
 
             <h2 id = "section-3" className="text-left font-medium text-2xl pb-4">
+              {" "}
               Audience
             </h2>
             <p className="text-left pb-4">
+              {" "}
               Designed specifically for the academia, our platform reimagines the research process. 
               At its core, an AI-driven search engine adeptly suggests research papers that align closely with your queries, ensuring you find the most relevant academic resources. 
               Beyond a mere search tool, our platform integrates a text editor for seamless annotation and review of these papers, 
@@ -76,44 +84,53 @@ const About = () => {
 
             {/* Extra Sections to test sidebar works*/}
             <h2 id = "section-4" className="text-left font-medium text-2xl pb-4">
+              {" "}
               Features
             </h2>
 
             <h2 className="text-left  text-xl font-medium pb-4 px-4"> 
-            Search 
+              {" "}
+              Search 
             </h2>
         
             <p className="text-left pb-4 px-4">
-            Dive into a world of research articles with our AI-enhanced search! 
-            Tailored for searching academic research papers, our engine doesn't just find keywords; 
-            it understands your research needs, connecting you with the papers most relevant to your academic inquiries.
+              {" "}
+              Dive into a world of research articles with our AI-enhanced search! 
+              Tailored for searching academic research papers, our engine doesn't just find keywords; 
+              it understands your research needs, connecting you with the papers most relevant to your academic inquiries.
             </p>
 
             <h2 className="text-left  text-xl font-medium pb-4 px-4"> 
+              {" "}
               Text Editor
             </h2>
         
             <p className="text-left pb-4 px-4">
-            Our integrated text editor is designed for easy annotation, organization, and editing. It works seamlessly within the platform, 
-            ensuring your thoughts are captured alongside your sources. It includes annotation features such as: 
-            bolding titles, text Size Increase, highlighting, color coding, and ability blocks.
+              {" "}
+              Our integrated text editor is designed for easy annotation, organization, and editing. It works seamlessly within the platform, 
+              ensuring your thoughts are captured alongside your sources. It includes annotation features such as: 
+              bolding titles, text Size Increase, highlighting, color coding, and ability blocks.
             </p>
             
             <h2 className="text-left  text-xl font-medium pb-4 px-4"> 
+              {" "}
               AI Chatbot Window
             </h2>
         
             <p className="text-left pb-4 px-4">
+              {" "}
               Need a Study Buddy? Use our AI Chatbot as your intelligent assistant, ready to generate summaries, 
               suggest citations, and offer insights on your findings! 
               It's like having a research partner that's well-versed in every subject.
             </p>
 
             <h2 className="text-left  text-xl font-medium pb-4 px-4"> 
+              {" "}
               Visualization Graph
             </h2>
         
             <p className="text-left pb-5 px-4">
+              {" "}
               Our Visualization Graph transforms your search into a visual map of connected research topics. 
               Each graph represents a research paper and represnts how they are related with each other. 
             </p>
@@ -122,22 +139,25 @@ const About = () => {
             <h2 id = "section-5" className="text-left font-medium text-2xl pb-4 pt-2" > FAQ </h2>
             <Accordion className= "pb-4 px-4 " type="single" collapsible>
               <AccordionItem className = "border-box rounded-2xl shadow-lg p-4 px-4" value="item-1">
-                <AccordionTrigger>How does the AI search engine work?</AccordionTrigger>
+                <AccordionTrigger>{" "}How does the AI search engine work?</AccordionTrigger>
                 <AccordionContent>
-                Our search engine uses deep learning to understand the context of your search query. 
-                It then suggests research papers and articles that are closely related to your topic, 
-                offering you a curated list of relevant research.
+                  {" "}
+                  Our search engine uses deep learning to understand the context of your search query. 
+                  It then suggests research papers and articles that are closely related to your topic, 
+                  offering you a curated list of relevant research.
                 </AccordionContent>
               </AccordionItem>
               <AccordionItem className = "border-box rounded-2xl shadow-lg p-4 px-4" value="item-2"> 
-                <AccordionTrigger>Can I save and annotate papers directly on the platform?</AccordionTrigger>
+                <AccordionTrigger>{" "}Can I save and annotate papers directly on the platform?</AccordionTrigger>
                   <AccordionContent>
+                    {" "}
                     Yes. Our integrated text editor allows you to save, annotate, and manage your research papers directly within the platform
                   </AccordionContent>
               </AccordionItem>
               <AccordionItem className = "border-box rounded-2xl shadow-lg p-4 px-4" value="item-2"> 
-                <AccordionTrigger>Is Arthas AI suitable for all academic disciplines?</AccordionTrigger>
+                <AccordionTrigger>{" "}Is Arthas AI suitable for all academic disciplines?</AccordionTrigger>
                   <AccordionContent>
+                    {" "}
                     Absolutely! Our platform caters to a wide range of academic fields, 
                     offering extensive coverage of diverse research areas to support comprehensive scholarly work.
                     </AccordionContent>
